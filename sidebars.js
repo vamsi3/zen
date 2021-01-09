c9d7e210b038b7f88ccd37c258979833eee1392c
Vamsi Krishna Reddy <vamsi3@iitb.ac.in>
2021-01-10 00:30:47 +0900

    Temporary fix for sidebar

--- sidebars.js
+++ sidebars.js
@@ -2,33 +2,29 @@ const fs = require("fs")
 const path = require("path")
 
 toTitleCase = str => {
-  var upper = true
-  var newStr = ""
+  var upper = true, newStr = ""
   for (const c of str) {
-      if (c == "-") {
-          upper = true
-          newStr += " "
-      } else {
-        newStr += upper ? c.toUpperCase() : c
-        upper = false
-      }
+    if (c == "-") {
+      newStr += " "
+      upper = true
+    } else {
+      newStr += upper ? c.toUpperCase() : c
+      upper = false
+    }
   }
   return newStr
 }
 
 sidebarConfig = (base, dir = '') => {
-  var config = []
+  var fileConfig = [], dirConfig = []
   fs.readdirSync(path.join(base, dir)).forEach(file => {
     const ext = path.extname(file)
     if (['.md', '.mdx'].includes(ext)) {
-      config.push(
-        {
-          type: 'doc',
-          id: path.join(dir, path.basename(file, ext)),
-        }
+      fileConfig.push(
+        path.join(dir, path.basename(file, ext)),
       )
     } else {
-      config.push(
+      dirConfig.push(
         {
           type: 'category',
           label: toTitleCase(file),
@@ -38,7 +34,7 @@ sidebarConfig = (base, dir = '') => {
       )
     }
   })
-  return config
+  return [...fileConfig, ...dirConfig]
 }
 
 module.exports = {
