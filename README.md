1d37a13ae8efae60662292327cb9de2a398d9907
Vamsi Krishna Reddy Satti <vamsi3@outlook.com>
2022-01-05 00:13:58 +0900

    Always only one commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project Zen
+
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/vamsi3/zen/Deploy%20website%20CI?style=flat-square)
+![Website](https://img.shields.io/website?style=flat-square&url=https%3A%2F%2Fvamsi3.github.io%2Fzen%2F)
+![GitHub repo size](https://img.shields.io/github/repo-size/vamsi3/zen?style=flat-square)
+![GitHub](https://img.shields.io/github/license/vamsi3/zen?style=flat-square)
---
+++ docusaurus.config.js
@@ -0,0 +1,146 @@
+const remarkMath = require("remark-math")
+const rehypeKatex = require("rehype-katex")
+
+module.exports = {
+  title: 'Project ZEN',
+  tagline: 'A collection of my knowledge',
+  url: 'https://vamsi3.github.io',
+  baseUrl: '/zen/',
+  organizationName: 'vamsi3',
+  projectName: 'zen',
+  favicon: 'img/favicon.ico',
+  trailingSlash: true,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET',
+      crossorigin: 'anonymous',
+    },
+  ],
+  scripts: [
+    {
+      src: "https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.js",
+      integrity: "sha256-FyuFDgL3AT2Wi7dlv82fSVvxe2rPx1rRSVtMOWeRp6k=",
+      crossorigin: 'anonymous',
+    }
+  ],
+  themeConfig: {
+    hideableSidebar: true,
+    announcementBar: {
+      id: 'support-me',
+      content: '⭐️ If you like Project ZEN, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/vamsi3/zen">GitHub</a>! ⭐️',
+    },
+    navbar: {
+      title: 'Project ZEN',
+      logo: {
+        alt: 'Project ZEN Logo',
+        src: 'img/logo.png',
+        srcDark: 'img/logo_dark.png'
+      },
+      items: [
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'cp/home',
+          label: 'Data Structures / Algorithms',
+        },
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'cpp/home',
+          label: 'C++20',
+        },
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'dip/home',
+          label: 'Image Processing',
+        },
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'dl/home',
+          label: 'AI / Machine / Deep Learning',
+        },
+        {
+          label: 'CP Profiles',
+          position: 'right',
+          items: [
+            {
+              label: 'Codeforces',
+              href: 'https://codeforces.com/profile/vamsi3'
+            },
+            {
+              label: 'Codechef',
+              href: 'https://www.codechef.com/users/vamsikrishna_3',
+            },
+            {
+              label: 'AtCoder',
+              href: 'https://atcoder.jp/users/vamsi3',
+            }
+          ],
+        },
+        {
+          href: 'https://github.com/vamsi3/zen',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ],
+      hideOnScroll: true,
+    },
+    footer: {
+      logo: {
+        alt: 'Project ZEN',
+        src: 'img/logo_dark.png',
+        href: 'https://vamsi3.github.io',
+      },
+      style: 'dark',
+      copyright: `© ${new Date().getFullYear()} Satti Vamsi Krishna Reddy • All rights reserved • Built with Docusaurus`,
+    },
+    prism: {
+      defaultLanguage: 'cpp',
+      additionalLanguages: ['python', 'java', 'kotlin'],
+      // theme: require('prism-react-renderer/themes/github'),
+      theme: require('./src/themes/ayuDark.js'),
+      darkTheme: require('./src/themes/ayuDark.js'),
+    },
+    // algolia: {
+    //   apiKey: '',
+    //   indexName: '',
+    //   contextualSearch: true,
+    // },
+    // gtag: {
+    //   trackingID: 'G-35E7VC4V7P',
+    //   // Optional fields.
+    //   anonymizeIP: true, // Should IPs be anonymized?
+    // }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarCollapsed: false,
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/vamsi3/zen/edit/master/',
+          routeBasePath: '/',
+          showLastUpdateTime: true,
+          numberPrefixParser: false,
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [[rehypeKatex, {output: 'html', minRuleThickness: 0.06, strict: false}]],
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+        },
+      },
+    ],
+  ]
+};
